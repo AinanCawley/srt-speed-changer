c8e5481b952ab92196080f2b2a4154cab5090668
AinanCawley <ainan.c.cawley@gmail.com>
2022-04-23 13:41:03 +0800

    Finish takeStringAndReturnAdjustedString function

--- script.js
+++ script.js
@@ -3,15 +3,22 @@ function takeStringAndReturnAdjustedString(srtString, speedRatioNumber)
     const timecodeRegex = /\d\d:\d\d:\d\d,\d\d\d/mg;
 
     let timecodeMatches;
+    let adjustedSrtString = srtString;
 
     while( (timecodeMatches = timecodeRegex.exec(srtString)) !== null )
     {
+        adjustedSrtString = replaceAt( srtString, timecodeMatches[0], timecodeAdjust(timecodeMatches[0]), timecodeMatches.index, (timecodeMatches.index + 12) )
         
     }
 
     
 
-    /* return adjustedSrtString; */
+    return adjustedSrtString;
+}
+
+function replaceAt( inputString, searchForThis, replaceWithThis, indexStart, indexEnd )
+{
+    return inputString.slice(0, indexStart) + inputString.slice(indexStart, indexEnd).replace(searchForThis, replaceWithThis) + inputString.slice(indexEnd);
 }
 
 function convertTimecodeToSeconds(timecodeString)
